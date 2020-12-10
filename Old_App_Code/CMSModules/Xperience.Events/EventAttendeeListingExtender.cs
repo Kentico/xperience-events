fc375385f4dd272a4e681792a5b4ca708d339aa3
Eric Dugre <EricD@kentico.com>
2020-12-10 11:04:31 -0500

    Initial commit

---
+++ Old_App_Code/CMSModules/Xperience.Events/EventAttendeeListingExtender.cs
@@ -0,0 +1,66 @@
+﻿using CMS.Base.Web.UI;
+using CMS.DocumentEngine;
+using CMS.DocumentEngine.Types.Xperience;
+using CMS.Helpers;
+using CMS.SiteProvider;
+using CMS.UIControls;
+using System;
+using System.Collections;
+using System.Globalization;
+using System.Linq;
+
+namespace Xperience.Events
+{
+    public class EventAttendeeListingExtender : ControlExtender<UniGrid>
+    {
+        private Event Event { get; set; }
+
+        public override void OnInit()
+        {
+            var nodeID = QueryHelper.GetInteger("nodeid", 0);
+            var culture = QueryHelper.GetString("culture", SiteContext.CurrentSite.DefaultVisitorCulture);
+            Event = DocumentHelper.GetDocuments<Event>()
+                .WhereEquals("NodeID", nodeID)
+                .Culture(culture)
+                .OnCurrentSite()
+                .TopN(1)
+                .FirstOrDefault();
+
+            if(!Event.EventRequiresRegistration)
+            {
+                Control.Visible = false;
+                Control.HeaderActions.Visible = false;
+                
+                var heading = new LocalizedHeading();
+                heading.Level = 4;
+                heading.ResourceString = "xperience.events.registrationdisabled";
+                Control.Parent.Controls.Add(heading);
+            }
+            else
+            {
+                Control.ShowObjectMenu = false;
+                Control.OnAfterRetrieveData += Control_OnAfterRetrieveData;
+            }
+        }
+
+        private System.Data.DataSet Control_OnAfterRetrieveData(System.Data.DataSet ds)
+        {
+            var attendeeCount = ds.Tables[0].Rows.Count;
+            var capacity = Event.EventCapacity;
+
+            var formatter = new NumberFormatInfo();
+            formatter.NumberDecimalDigits = 1;
+            var percentage = capacity > 0 ?
+                Convert.ToDouble((Convert.ToDouble(attendeeCount) / Convert.ToDouble(capacity)) * 100) :
+                0;
+            Control.MessagesPlaceHolder.ShowInformation( $"<b>Capacity</b>: {capacity} <b>Attendees</b>: {attendeeCount} <b>Registration</b>: {percentage.ToString("N", formatter)}%", persistent: true);
+            
+            if(attendeeCount >= capacity && !Event.EventAllowOverCapacity)
+            {
+                Control.HeaderActions.Enabled = false;
+            }
+
+            return ds;
+        }
+    }
+}
---
+++ Old_App_Code/CMSModules/Xperience.Events/EventsModule.cs
@@ -0,0 +1,38 @@
+﻿using CMS;
+using CMS.Core;
+using CMS.DataEngine;
+using Events;
+using System.Linq;
+using Xperience.Events;
+
+[assembly: RegisterModule(typeof(EventsModule))]
+namespace Xperience.Events
+{
+    public class EventsModule : Module
+    {
+        public EventsModule() : base(nameof(EventsModule)) { }
+
+        protected override void OnInit()
+        {
+            base.OnInit();
+            EventAttendeeInfo.TYPEINFO.Events.Insert.Before += Insert_Before;
+        }
+
+        private void Insert_Before(object sender, ObjectEventArgs e)
+        {
+            // Don't allow duplicate registrations to the same event
+            var attendee = e.Object as EventAttendeeInfo;
+            var existingAttendee = EventAttendeeInfoProvider.ProviderObject.Get()
+                .WhereEquals("ContactID", attendee.ContactID)
+                .WhereEquals("NodeID", attendee.NodeID)
+                .TopN(1)
+                .FirstOrDefault();
+
+            if(existingAttendee != null)
+            {
+                e.Cancel();
+                throw new System.Exception("Attendee already registered for event.");
+            }
+        }
+    }
+}
